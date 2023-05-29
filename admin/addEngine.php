fa3e4cd6c6e497fd38ad46b2d4645dca3b08825e
Max PC <maximilianjenntsch7@gmail.com>
2023-05-29 19:43:48 +0200

    seperated add and uploadEngine scripts

--- admin/addEngine.php
+++ admin/addEngine.php
@@ -12,62 +12,6 @@ ob_start(); // Pufferung starten
 
 // Geschützter Inhalt der Seite
 
-require("../DBConnect.php");
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $baureihe = "999 999";
-    $name = "";
-    $owner = "";
-
-    if(isset($_POST['baureihe'])){
-        $baureihe = $_POST['baureihe'];
-    }
-
-    if(isset($_POST['name'])){
-        $name = $_POST['name'];
-    }
-
-    if(isset($_POST['owner'])){
-        $owner = $_POST['owner'];
-    }
-
-    if (isset($_FILES['image'])) {
-        $targetDir = '../data/images/';
-        $ext = pathinfo(basename($_FILES['image']['name']), PATHINFO_EXTENSION);
-        $targetFile = $targetDir . $baureihe . '.' . $ext;
-
-        if (move_uploaded_file($_FILES['image']['tmp_name'], $targetFile)) {
-            /*$sql = "INSERT INTO Engines (Baureihe, Name, Owner)
-            VALUES (". $baureihe . ", " . $name . ", " . $owner . ");";
-            $surpress = $DBASE->query();
-            */
-            $sql = "INSERT INTO Engines (Baureihe, Name, Owner) VALUES (?,?,?);";
-            $stmt= $DBASE->prepare($sql);
-            $stmt->execute([$baureihe, $name, $owner]);    
-
-            $response = array(
-                'success' => true,
-                'message' => 'Bild erfolgreich hochgeladen.'
-            );
-        } else {
-            $response = array(
-                'success' => false,
-                'message' => 'Fehler beim Hochladen des Bildes.'
-            );
-        }
-    } else {
-        $response = array(
-            'success' => false,
-            'message' => 'Kein Bild ausgewählt.'
-        );
-    }
-
-    //ob_end_clean(); // Puffer leeren
-    header('Content-Type: application/json');
-    echo json_encode($response);
-    exit; // Beende das Skript hier, um das Frontend nicht auszuführen
-} 
-
 ?>
 
 
@@ -133,7 +77,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             formData.append("owner", owner.value);
 
             var xhr = new XMLHttpRequest();
-            xhr.open("POST", "<?php $_SERVER["PHP_SELF"] ?>", true);
+            xhr.open("POST", "uploadEngine.php", true);
             xhr.onload = function() {
                 if (xhr.status === 200) {
                     var response = JSON.parse(xhr.responseText);
---
+++ admin/uploadEngine.php
@@ -0,0 +1,59 @@
+<?php
+
+require("../DBConnect.php");
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $baureihe = "999 999";
+    $name = "";
+    $owner = "";
+
+    if(isset($_POST['baureihe'])){
+        $baureihe = $_POST['baureihe'];
+    }
+
+    if(isset($_POST['name'])){
+        $name = $_POST['name'];
+    }
+
+    if(isset($_POST['owner'])){
+        $owner = $_POST['owner'];
+    }
+
+    if (isset($_FILES['image'])) {
+        $targetDir = '../data/images/';
+        $ext = pathinfo(basename($_FILES['image']['name']), PATHINFO_EXTENSION);
+        $targetFile = $targetDir . $baureihe . '.' . $ext;
+
+        if (move_uploaded_file($_FILES['image']['tmp_name'], $targetFile)) {
+            /*$sql = "INSERT INTO Engines (Baureihe, Name, Owner)
+            VALUES (". $baureihe . ", " . $name . ", " . $owner . ");";
+            $surpress = $DBASE->query();
+            */
+            $sql = "INSERT INTO Engines (Baureihe, Name, Owner) VALUES (?,?,?);";
+            $stmt= $DBASE->prepare($sql);
+            $stmt->execute([$baureihe, $name, $owner]);    
+
+            $response = array(
+                'success' => true,
+                'message' => 'Bild erfolgreich hochgeladen.'
+            );
+        } else {
+            $response = array(
+                'success' => false,
+                'message' => 'Fehler beim Hochladen des Bildes.'
+            );
+        }
+    } else {
+        $response = array(
+            'success' => false,
+            'message' => 'Kein Bild ausgewählt.'
+        );
+    }
+
+    //ob_end_clean(); // Puffer leeren
+    header('Content-Type: application/json');
+    echo json_encode($response);
+    exit; // Beende das Skript hier, um das Frontend nicht auszuführen
+} 
+
+?>
