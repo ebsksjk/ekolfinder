7b4e2348917a3f391134b0046fc516cc815cd817
Max PC <maximilianjenntsch7@gmail.com>
2023-06-19 19:21:09 +0200

    added composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'maplemonarchs/ekolfinder',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'f6527c1cf376e8272d44fda2e4aec79335302b56',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'maplemonarchs/ekolfinder' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'f6527c1cf376e8272d44fda2e4aec79335302b56',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
